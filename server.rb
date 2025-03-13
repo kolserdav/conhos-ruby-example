a87033e8f50ab7d56c0aacc20f28e818ea236b56
kolserdav <serega12101983@gmail.com>
2025-03-13 12:41:40 +0700

    add show IP

--- server.rb
+++ server.rb
@@ -5,6 +5,8 @@ port = ENV['PORT'] || 3000
 server = WEBrick::HTTPServer.new(:Port => port)
 
 server.mount_proc '/' do |req, res|
+  client_ip = req.header['x-forwarded-for'] ? req.header['x-forwarded-for'].first : req.remote_ip
+  puts "Client IP: #{client_ip}"
   res.body = 'Hello, Ruby!'
 end
 
